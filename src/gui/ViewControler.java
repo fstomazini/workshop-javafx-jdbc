e5827213d0348d70c1c4a06aa26e6e0dda76ffaa
fstomazini <felipestomazini@gmail.com>
2020-08-12 00:44:30 -0300

    Initial setup

---
+++ src/gui/ViewControler.java
@@ -0,0 +1,21 @@
+package gui;
+
+
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ViewControler implements Initializable {
+gitgit pull
+
+
+
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+
+    }
+}
